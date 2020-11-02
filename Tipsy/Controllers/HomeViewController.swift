0882bb52631619075d7e1b2198c6ce94ef1138bc
m-zaink <sidsmith9@gmail.com>
2020-11-02 23:42:05 +0530

    Add all basic IBOutlets and IBActions to components on HomeScren

--- Tipsy/Controllers/HomeViewController.swift
+++ Tipsy/Controllers/HomeViewController.swift
@@ -11,32 +11,107 @@ import UIKit
 class HomeViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        billAmountTextField.delegate = self
-    }
-
-    @IBAction func calculateButtonPressed(_ sender: UIButton) {
-        if let enteredAmount = billAmountTextField.text {
-            print(enteredAmount)
+    @IBOutlet weak var zeroTipButton: UIButton!
+    @IBOutlet weak var tenthTipButton: UIButton!
+    @IBOutlet weak var twentiethTipButton: UIButton!
+    
+    @IBOutlet weak var splitCountLabel: UILabel!
+    
+    @IBAction func onCalculateButtonPressed(_ sender: UIButton) {
+        guard let enteredAmountAsString = billAmountTextField.text, !enteredAmountAsString.isEmpty else {
+            let alert = createUIAlertForEmptyBillTotal()
+            self.present(alert, animated: true, completion: nil)
+            return
         }
+        
+        guard  let enteredAmount = Float(enteredAmountAsString) else {
+            let alert = createUIAlertForIncorrectBillTotal()
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        print(enteredAmount)
     }
     
-    @IBAction func tapGestureRecognized(_ sender: UITapGestureRecognizer) {
-        if let enteredAmountAsString = billAmountTextField.text, let enteredAmount = Int(enteredAmountAsString) {
-            view.endEditing(true)
+    @IBAction func onTapGestureRecognized(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onTipButtonPressed(_ sender: UIButton) {
+        guard let currentTitle = sender.currentTitle else {
+            return
+        }
+        
+        guard let tipPercentage = Double(currentTitle.replacingOccurrences(of: "%", with: "")) else {
+            return
+        }
+        
+        unselectCurrentlySelectedTipButton()
+        
+        sender.isSelected = true
+        
+        print(tipPercentage)
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        splitCountLabel.text = String(format: "%.0f", sender.value)
+    }
+}
+
+
+// MARK:- Auxilliary methods
+extension HomeViewController {
+    func createUIAlertForEmptyBillTotal() -> UIAlertController {
+        let alert = UIAlertController(
+            title: "Bill total cannot be empty",
+            message: "Please enter your bill amount",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: UIAlertAction.Style.default) {
+            _ in
+            self.billAmountTextField.becomeFirstResponder()
+        }
+        
+        alert.addAction(okAction)
+        
+        return alert
+    }
+    
+    func createUIAlertForIncorrectBillTotal() -> UIAlertController {
+        let alert = UIAlertController(
+            title: "Bill total is incorrect",
+            message: "Please enter a valid bill amount",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: UIAlertAction.Style.default) {
+            _ in
+            self.billAmountTextField.becomeFirstResponder()
+        }
+        
+        alert.addAction(okAction)
+        
+        return alert
+    }
+    
+    func unselectCurrentlySelectedTipButton() {
+        if zeroTipButton.isSelected {
+            zeroTipButton.isSelected = false
+        } else if tenthTipButton.isSelected {
+            tenthTipButton.isSelected = false
         } else {
-            print("Invalid number added so far")
+            twentiethTipButton.isSelected = false
         }
     }
 }
 
-extension HomeViewController : UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let enteredAmount = textField.text {
-            print(enteredAmount)
-        }
+fileprivate extension String {
+    var toTip: String {
+        return self.replacingOccurrences(of: "%", with: "")
     }
 }
